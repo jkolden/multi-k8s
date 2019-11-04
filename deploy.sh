a9226286aefde8661d5b66aa088f9d91b32eda99
John Kolden <john.kolden@gmail.com>
2019-11-04 08:41:00 -0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+docker build -t jkolden/multi-client:latest -t jkolden/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jkolden/multi-server:latest -t jkolden/multi-server:$SHA -f ./server/Dockerfile ./server
+docker push jkolden/multi-client:latest
+docker push jkolden/multi-server:latest
+docker push jkolden/multi-server:latest
+docker push jkolden/multi-client:$SHA
+docker push jkolden/multi-server:$SHA
+kubectl appy -f k8s
+kubectl set image deployments/server-deployment server=jkolden/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jkolden/multi-client:$SHA
